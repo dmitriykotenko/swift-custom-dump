1f6327e417837162d137bc145a04c886bfaee764
Brandon Williams <mbrandonw@hey.com>
2024-06-21 19:27:24 -0400

    wip

--- Package@swift-6.0.swift
+++ Package@swift-6.0.swift
@@ -17,13 +17,16 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.0.0")
+    //.package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.0.0"),
+    //.package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", branch: "swift-testing"),
+    .package(path: "../xctest-dynamic-overlay"),
   ],
   targets: [
     .target(
       name: "CustomDump",
       dependencies: [
-        .product(name: "XCTestDynamicOverlay", package: "xctest-dynamic-overlay")
+        .product(name: "XCTestDynamicOverlay", package: "xctest-dynamic-overlay"),
+        .product(name: "TestingDynamicOverlay", package: "xctest-dynamic-overlay"),
       ]
     ),
     .testTarget(
